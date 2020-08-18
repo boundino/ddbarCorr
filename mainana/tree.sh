7f1b865a0d84e6559fe5f2dc5c7ccbd36ca9a454
Tzu-An Sheng <tasheng@mit.edu>
2020-08-18 23:02:37 +0800

    Use the standard path for root files

--- mainana/tree.sh
+++ mainana/tree.sh
@@ -9,7 +9,7 @@ pt2max=( 999 999 999 999 999 999 999 999 999 999)
 dy=(     1 1 1 1 1 1 1 1 1 1)
 cmin=(   0 0 0 0 0 0 0 0 0 0)
 cmax=(   80 80 80 80 80 80 80 80 80 80)
-label=4pi
+label=2pi
 
 inputdata=DntupleRun2018/skim_d0ana_PbPb2018_HIMinimumBias.root
 inputmc=rootfiles/masstpl_PbPb.root
@@ -28,6 +28,7 @@ cmake ..
 make || exit
 cd ..
 
+mkdir -p plots
 [[ -f $eff ]] || build/ddbar_efficiency $genmc $eff
 
 RUN_SAVETREE=${1:-0}
@@ -37,5 +38,5 @@ for rr in ${run[@]}
 do
     outputdir=dd_cent${cmin[rr]}-${cmax[rr]}_pt${pt1min[rr]}-${pt1max[rr]}_y${dy[rr]/'.'/'p'}_${label}
     [[ $RUN_SAVETREE -eq 1 ]] && { build/ddbar_savetree $inputdata $inputmc $outputdir ${pt1min[rr]} ${pt1max[rr]} ${pt2min[rr]} ${pt2max[rr]} ${dy[rr]} ${cmin[rr]} ${cmax[rr]} $label $eff $swapmc $mctree; }
-    [[ $RUN_2DFIT -eq 1 ]] && { build/ddbar_2dfit "$dpairtree" "rootfiles/$outputdir/${swaptree}" $outputdir ; }
+    [[ $RUN_2DFIT -eq 1 ]] && { build/ddbar_2dfit "rootfiles/$outputdir/$dpairtree" "rootfiles/$outputdir/${swaptree}" $outputdir ; }
 done
