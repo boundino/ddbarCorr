95da6a9a6df329c4488d9eb9b31280a64b08d016
Tzu-An Sheng <tasheng@mit.edu>
2020-08-31 19:47:17 +0800

    Save generated kinematic variables in MC

--- mainana/ddbar_savetree.cc
+++ mainana/ddbar_savetree.cc
@@ -166,10 +166,16 @@ void ddbar_swapmc(std::string swapmc, std::string output, std::string inputeff,
   TTreeReaderArray<Float_t> mass_d0(reader, "mass");
   TTreeReaderArray<Float_t> flavor(reader, "flavor");
   TTreeReaderArray<Bool_t> isSwap(reader, "isSwap");
+  TTreeReaderValue<Int_t> candSize_gen(reader, "candSize_gen");
+  TTreeReaderArray<Float_t> pT_gen(reader, "pT_gen");
+  TTreeReaderArray<Float_t> eta_gen(reader, "eta_gen");
+  TTreeReaderArray<Float_t> y_gen(reader, "y_gen");
   TTreeReaderArray<Int_t> da1(reader, "DauID1_gen");
   TTreeReaderArray<Int_t> da2(reader, "DauID2_gen");
   TTreeReaderArray<Float_t> ptd1(reader, "pTD1");
   TTreeReaderArray<Float_t> ptd2(reader, "pTD2");
+  TTreeReaderArray<Float_t> pterrd1(reader, "pTerrD1");
+  TTreeReaderArray<Float_t> pterrd2(reader, "pTerrD2");
   TTreeReaderArray<Float_t> etad1(reader, "EtaD1");
   TTreeReaderArray<Float_t> etad2(reader, "EtaD2");
 
@@ -185,15 +191,22 @@ void ddbar_swapmc(std::string swapmc, std::string output, std::string inputeff,
   TFile *outf = new TFile(outputname.c_str(), "recreate");
   Float_t m1, m2, weight;
   Float_t y1, y2;
+  Float_t pt1, pt2;
   Float_t dphi;
   Int_t iPhi;
   Bool_t signal;
   Bool_t isSwap1, isSwap2;
   Bool_t sameK;
+  Int_t genda1, genda2;
   Float_t pt1da1, pt1da2;
   Float_t pt2da1, pt2da2;
+  Float_t pterr1da1, pterr1da2;
+  Float_t pterr2da1, pterr2da2;
   Float_t eta1da1, eta1da2;
   Float_t eta2da1, eta2da2;
+  Float_t dgen1, dgen2;
+  Int_t lund1da1, lund1da2;
+  Int_t lund2da1, lund2da2;
   TTree* swaptree = new TTree("swapdp", "swapped D0 pair");
   swaptree->Branch("m1", &m1, "m1/F");
   swaptree->Branch("m2", &m2, "m2/F");
@@ -203,6 +216,12 @@ void ddbar_swapmc(std::string swapmc, std::string output, std::string inputeff,
   swaptree->Branch("signal", &signal, "signal/B");
   swaptree->Branch("y1", &y1, "y1/F");
   swaptree->Branch("y2", &y2, "y2/F");
+  swaptree->Branch("pt1", &pt1, "pt1/F");
+  swaptree->Branch("pt2", &pt2, "pt2/F");
+  swaptree->Branch("pterr1da1", &pterr1da1, "pterr1da1/F");
+  swaptree->Branch("pterr1da2", &pterr1da2, "pterr1da2/F");
+  swaptree->Branch("pterr2da1", &pterr2da1, "pterr2da1/F");
+  swaptree->Branch("pterr2da2", &pterr2da2, "pterr2da2/F");
   swaptree->Branch("isSwap1", &isSwap1, "isSwap1/B");
   swaptree->Branch("isSwap2", &isSwap2, "isSwap2/B");
   swaptree->Branch("sameK", &sameK, "sameK/B");
@@ -214,6 +233,14 @@ void ddbar_swapmc(std::string swapmc, std::string output, std::string inputeff,
   swaptree->Branch("pt2da2", &pt2da2, "pt2da2/F");
   swaptree->Branch("eta1da2", &eta1da2, "eta1da2/F");
   swaptree->Branch("eta2da2", &eta2da2, "eta2da2/F");
+
+  // gen level
+  swaptree->Branch("dgen1", &dgen1, "dgen1/F");
+  swaptree->Branch("dgen2", &dgen2, "dgen2/F");
+  swaptree->Branch("lund1da1", &lund1da1, "lund1da1/I");
+  swaptree->Branch("lund2da1", &lund2da1, "lund2da1/I");
+  swaptree->Branch("lund1da2", &lund1da2, "lund1da2/I");
+  swaptree->Branch("lund2da2", &lund2da2, "lund2da2/I");
   TH1I* cat = new TH1I("cat", "category; D#bar{D} type; Events", 3, 0, 3);
   TH1I *nCand = new TH1I(
       "nCand", "nCand;# of gen D(#bar{D}) candidates; Events", 10, 0, 10);
@@ -274,20 +301,57 @@ void ddbar_swapmc(std::string swapmc, std::string output, std::string inputeff,
         y2 = y[i2];
         isSwap1 = isSwap[i1];
         isSwap2 = isSwap[i2];
+        pt1 = pT[i1];
+        pt2 = pT[i2];
         pt1da1 = ptd1[i1];
         pt1da2 = ptd2[i1];
         pt2da1 = ptd1[i2];
         pt2da2 = ptd2[i2];
+        pterr1da1 = pterrd1[i1];
+        pterr1da2 = pterrd2[i1];
+        pterr2da1 = pterrd1[i2];
+        pterr2da2 = pterrd2[i2];
         eta1da1 = etad1[i1];
         eta1da2 = etad2[i1];
         eta2da1 = etad1[i2];
         eta2da2 = etad2[i2];
         // weight = scale;
+        // match gen particles
+        lund1da1 = -1;
+        lund1da2 = -1;
+        lund2da1 = -1;
+        lund2da2 = -1;
+
+        auto distance = [](float pt, float ptgen, float y, float ygen) {
+          return std::pow(fabs(pt - ptgen) / 0.03, 2) +
+            std::pow(fabs(y - ygen) / 0.002, 2);
+        };
+        std::vector<float> dist1;
+        std::vector<float> dist2;
+        for (int igen = 0; igen < *candSize_gen; ++igen) {
+          dist1.push_back(distance(pT[i1], pT_gen[igen], y[i1], y_gen[igen]));
+          dist2.push_back(distance(pT[i2], pT_gen[igen], y[i2], y_gen[igen]));
+        }
+        std::vector<float>::iterator min1 = std::min_element(dist1.begin(), dist1.end());
+        int igen1 = std::distance(dist1.begin(), min1);
+        std::vector<float>::iterator min2 = std::min_element(dist2.begin(), dist2.end());
+        int igen2 = std::distance(dist2.begin(), min2);
+        lund1da1 = da1[igen1];
+        lund1da2 = da2[igen1];
+        lund2da1 = da1[igen2];
+        lund2da2 = da2[igen2];
+        dgen1 = dist1[igen1];
+        dgen2 = dist2[igen2];
+
+        if (lund1da1 == -1 || lund2da1 == -1) {
+          std::cout << "Warning: no matching gen particle!" << "\n";
+        }
+
+
         if (iPhi < 0) {
           std::cout << __FUNCTION__ << ": error: invalid dphi calculated."
                     << std::endl;
         }
-        // only keep the swap event
         if (isSwap[iCand] ^ isSwap[jCand]) {
           signal = false;
           cat->Fill(1);
